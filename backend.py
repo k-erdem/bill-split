dc5b48e6e74f286aa924f1b05e1c104d35ad3f3d
Kaan Erdem <ke2365@columbia.edu>
2024-02-23 23:32:38 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,5 +2,6 @@ import easyocr
 
 
 reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
-result = reader.readtext('sample_image.jpg')
+result = reader.readtext('sample_image.jpeg')
 
+print(os.getcwd())
